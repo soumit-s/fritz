52b34420992e62c0bb6a6e20257cf1dada8cce29
soumit-s <soumit.srim@gmail.com>
2022-05-03 22:04:50 +0530

    For now, class_create returns the object(c), as it is.

---
+++ include/runtime/class.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "runtime/obj.h"
+
+extern Object* class_create(Object *c, Object *s, Value n);
---
+++ src/runtime/class.c
@@ -0,0 +1,5 @@
+#include "runtime/class.h"
+
+Object* class_create(Object *c, Object *s, Value n) {
+	return c;
+}
