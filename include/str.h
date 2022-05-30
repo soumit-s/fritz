d019595e6f6baeeea7e03d01b1d896f9fcf2d6ae
soumit-s <soumit.srim@gmail.com>
2022-05-30 20:35:01 +0530

    Added the STRING_NEW macro for initialization of string structs. Added to_cstring for creating null terminated strings from fritz strings i.e is the string struct. Added the STRING_CLONE macro for cloning a string into a new variable.

--- include/str.h
+++ include/str.h
@@ -9,12 +9,16 @@ struct fz_string {
 	size_t length;
 };
 
+// static initializer for string struct
+#define STRING_NEW(v, l) {.value = v, .length = l}
+
 extern string string_new(const char*, size_t);
 
 extern int string_eq(string, string);
 extern int string_eqc(string, const char*);
 
 extern string to_string(const char*);
+extern char* to_cstring(string s);
 
 extern long string_to_long(string);
 extern long string_hex_to_long(string);
@@ -24,3 +28,10 @@ extern long string_bin_to_long(string);
 extern double string_to_double(string);
 
 extern string string_to_string(string);
+
+#define STRING_PRINT(s) for(int i=0; i < (s).length; ++i) printf("%c", (s).value[i]);
+
+#define STRING_CLONE(s, c) \
+	string c = STRING_NEW(malloc(s.length * sizeof(char)), s.length); \
+	for (size_t n=s.length; n-- > 0;) ((char*)c.value)[n] = s.value[n];
+		
