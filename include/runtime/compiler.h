1cfac714020557a53587961396347dda2fef9500
soumit-s <soumit.srim@gmail.com>
2022-05-30 20:38:47 +0530

    Added the instance_compile_nodes function.

--- include/runtime/compiler.h
+++ include/runtime/compiler.h
@@ -4,6 +4,9 @@
 #include "pre/ast.h"
 #include "runtime/bcode.h"
 
+extern void instance_compile_nodes(Instance *i, AstNode *nodes, size_t n_nodes,
+                            BcodeBuffer *b, ConstantPoolCreator *p);
+
 extern void instance_compile_node(Instance*, AstNode, BcodeBuffer*, ConstantPoolCreator*);
 
 
