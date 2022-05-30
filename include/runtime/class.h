afa24b3c18c0b7b8df73eba6f62c22df96a93037
soumit-s <soumit.srim@gmail.com>
2022-05-30 20:36:25 +0530

    Added the class_create and class_instantiate functions for creating a Class object and for instantiating an object from a Class object respectively.

--- include/runtime/class.h
+++ include/runtime/class.h
@@ -2,4 +2,8 @@
 
 #include "runtime/obj.h"
 
+extern Value CONSTRUCTOR_METHOD_NAME, SUPER_PROP_NAME, CLASS_NAME_PROP_NAME;
+
 extern Object* class_create(Object *c, Object *s, Value n);
+
+extern Object* class_instantiate(Object *c);
